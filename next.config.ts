48035066cd4d2eed948b9280024ee8765d74e92a
InderN10 <midtulah2000@gmail.com>
2025-01-31 10:48:25 +0800

    mobileSilderFinished

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns : [
+      {
+        hostname: 'image.tmdb.org',
+        protocol: 'https'
+      }
+    ]
+  },
   env: {
     TMDB_BASE_URL: process.env.TMDB_BASE_URL || "",
     TMDB_IMAGE_SERVICE_URL: process.env.TMDB_IMAGE_SERVICE_URL || "",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,25 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import { useState } from "react";
 import { Movie } from "@/types/Movie-type";
+import Image from "next/image";
+import { Star } from 'lucide-react';
+import { Button} from "@/components/ui/button";
+import { Play } from 'lucide-react';
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+  // CardDescription,
+  // CardFooter,
+  // CardHeader,
+  // CardTitle,
 } from "@/components/ui/card";
-// import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 const TMDB_BASE_URL = process.env.TMDB_BASE_URL;
 const TMDB_API_TOKEN = process.env.TMDB_API_TOKEN;
@@ -29,11 +37,11 @@ export default function Home() {
         }
       );
       console.log(response);
-      setNowPlayingData(response.data.results)
     } catch (err) {
       console.log(err);
     }
   };
+
   const getNowPlayingMovieData = async () => {
     try {
       const response = await axios.get(
@@ -44,7 +52,8 @@ export default function Home() {
           },
         }
       );
-      console.log("this is now laying", response.data.results);
+      console.log("Now Playing Movies:", response.data.results);
+      setNowPlayingData(response.data.results);
     } catch (err) {
       console.log(err);
     }
@@ -58,18 +67,55 @@ export default function Home() {
   return (
     <div className="h-screen w-screen">
       <div>
-        <Card>
-          <CardHeader>
-            <CardTitle>{nowPlayingData}</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
+        <div>
+          <Carousel className="w-screen h-[1000px] flex flex-col p-0 ">
+            <CarouselContent>
+              {nowPlayingData.slice(0, 10).map((movie) => (
+                <CarouselItem key={movie.id}>
+                  <div>
+                    <Card>
+                      <CardContent className="flex  justify-center p-0">
+                        <div className="">
+                          <Image
+                            src={`https://image.tmdb.org/t/p/w300${movie.backdrop_path}`}
+                            width={1000}
+                            height={250}
+                            alt="Picture of the author"
+                          />
+                          <div className="p-5">
+                            <div className="flex justify-between">
+                              <div>
+                                <p>Now playing:</p>
+                                <h3 className="font-semibold text-2xl">
+                                  {movie.title}
+                                </h3>
+                              </div>
+                              <div className="flex gap-1 items-center">
+                                <Star className="text-yellow-400 w-7 h-7"/>
+                                {movie.vote_average}/10
+                              </div>
+                            </div>
+                            <div>
+                              <p className="font-normal text-sm leading-5 my-4 h-[100px] overflow-hidden overflow-y-auto">
+                                {movie.overview}
+                              </p>
+                            </div>
+                            <Button>
+                              <Play/>
+                              Watch trailer
+                            </Button>
+                          </div>
+                         
+                        </div>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+          </Carousel>
+        </div>
       </div>
     </div>
   );
